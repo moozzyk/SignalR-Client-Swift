2d4b86d22c3c1876dd3d51dcde3864ca6cf3ed30
moozzyk <moozzyk@gmail.com>
2017-02-23 20:08:30 -0800

    Initial commit

---
+++ SignalRClient/SignalRClient.h
@@ -0,0 +1,19 @@
+//
+//  SignalRClient.h
+//  SignalRClient
+//
+//  Created by Pawel Kadluczka on 2/23/17.
+//  Copyright © 2017 Pawel Kadluczka. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SignalRClient.
+FOUNDATION_EXPORT double SignalRClientVersionNumber;
+
+//! Project version string for SignalRClient.
+FOUNDATION_EXPORT const unsigned char SignalRClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignalRClient/PublicHeader.h>
+
+
