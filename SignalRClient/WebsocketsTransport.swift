22a0120454aaa274033f6f92b6b5159b3b545b84
moozzyk <moozzyk@gmail.com>
2017-02-23 20:17:36 -0800

    Adding dummy test

---
+++ SignalRClient/WebsocketsTransport.swift
@@ -0,0 +1,16 @@
+//
+//  WebsocketsTransport.swift
+//  SignalRClient
+//
+//  Created by Pawel Kadluczka on 2/23/17.
+//  Copyright © 2017 Pawel Kadluczka. All rights reserved.
+//
+
+import Foundation
+
+public class WebsocketsTransport {
+
+    public func test() -> String {
+        return "test string"
+    }
+}
---
+++ SignalRClientTests/WebsocketsTransportTests.swift
@@ -0,0 +1,36 @@
+//
+//  WebsocketsTransportTests.swift
+//  SignalRClient
+//
+//  Created by Pawel Kadluczka on 2/23/17.
+//  Copyright © 2017 Pawel Kadluczka. All rights reserved.
+//
+
+import XCTest
+@testable import SignalRClient
+
+class WebsocketsTransportTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        let wsTransport = WebsocketsTransport()
+        XCTAssertEqual("test string", wsTransport.test())
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
