53638b54f5514da5130abb1bc69b8520be0b8845
moozzyk <moozzyk@gmail.com>
2018-04-07 22:02:43 -0700

    Removing SocketRocket and Carthage

--- SignalRClient/WebsocketsTransport.swift
+++ SignalRClient/WebsocketsTransport.swift
@@ -7,49 +7,48 @@
 //
 
 import Foundation
-import SocketRocket
 
-public class WebsocketsTransport: NSObject, Transport, SRWebSocketDelegate {
-    var webSocket: SRWebSocket? = nil
+public class WebsocketsTransport: Transport {
+//    var webSocket: SRWebSocket? = nil
     public weak var delegate: TransportDelegate! = nil
 
     public func start(url:URL) {
-        self.webSocket = SRWebSocket(url: url)
-        self.webSocket!.delegate = self
-        self.webSocket!.open();
+//        self.webSocket = SRWebSocket(url: url)
+//        self.webSocket!.delegate = self
+//        self.webSocket!.open();
     }
 
     public func send(data: Data, sendDidComplete: (_ error: Error?) -> Void) {
-        do {
-            try webSocket?.send(data: data)
-            sendDidComplete(nil)
-        } catch {
-            sendDidComplete(error)
-        }
+//        do {
+//            try webSocket?.send(data: data)
+//            sendDidComplete(nil)
+//        } catch {
+//            sendDidComplete(error)
+//        }
     }
 
     public func close() {
-        webSocket?.close()
+//        webSocket?.close()
     }
 
-    public func webSocketDidOpen(_ webSocket: SRWebSocket) {
-        delegate?.transportDidOpen()
-    }
-
-    public func webSocket(_ webSocket: SRWebSocket, didReceiveMessageWith data: Data) {
-        delegate?.transportDidReceiveData(data)
-    }
-
-    public func webSocket(_ webSocket: SRWebSocket, didReceiveMessageWith string: String) {
-        delegate?.transportDidReceiveData(string.data(using: .utf8)!)
-    }
-
-    public func webSocket(_ webSocket: SRWebSocket, didCloseWithCode code: Int, reason: String?, wasClean: Bool) {
-        // TODO: Handle error codes
-        delegate?.transportDidClose(nil)
-    }
-
-    public func webSocket(_ webSocket: SRWebSocket, didFailWithError error: Error) {
-        delegate?.transportDidClose(error)
-    }  
+//    public func webSocketDidOpen(_ webSocket: SRWebSocket) {
+//        delegate?.transportDidOpen()
+//    }
+//
+//    public func webSocket(_ webSocket: SRWebSocket, didReceiveMessageWith data: Data) {
+//        delegate?.transportDidReceiveData(data)
+//    }
+//
+//    public func webSocket(_ webSocket: SRWebSocket, didReceiveMessageWith string: String) {
+//        delegate?.transportDidReceiveData(string.data(using: .utf8)!)
+//    }
+//
+//    public func webSocket(_ webSocket: SRWebSocket, didCloseWithCode code: Int, reason: String?, wasClean: Bool) {
+//        // TODO: Handle error codes
+//        delegate?.transportDidClose(nil)
+//    }
+//
+//    public func webSocket(_ webSocket: SRWebSocket, didFailWithError error: Error) {
+//        delegate?.transportDidClose(error)
+//    }
 }
