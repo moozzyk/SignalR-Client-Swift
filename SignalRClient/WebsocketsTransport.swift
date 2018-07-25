6796e4449393b27ef79c83303c7305638872766f
Miran Lesjak <miranl@gmail.com>
2018-07-25 13:36:36 +0200

    Prevent retain cycles

--- SignalRClient/WebsocketsTransport.swift
+++ SignalRClient/WebsocketsTransport.swift
@@ -21,54 +21,60 @@ public class WebsocketsTransport: Transport {
     }
 
     public func start(url: URL, options: HttpConnectionOptions) {
-        self.logger.log(logLevel: .info, message: "Starting WebSocket transport")
+        logger.log(logLevel: .info, message: "Starting WebSocket transport")
         var request = URLRequest(url: convertUrl(url: url))
-        
+
         populateHeaders(headers: options.headers, request: &request)
         setAccessToken(accessTokenProvider: options.accessTokenProvider, request: &request)
-        
+
         webSocket = WebSocket(request: request)
 
-        webSocket!.event.open = {
-            self.logger.log(logLevel: .info, message: "WebSocket open")
-            self.delegate?.transportDidOpen()
+        webSocket!.event.open = { [weak self] in
+            guard let welf = self else { return }
+            welf.logger.log(logLevel: .info, message: "WebSocket open")
+
+            welf.delegate?.transportDidOpen()
         }
 
-        webSocket!.event.close = { code, reason, clean in
-            self.logger.log(logLevel: .info, message: "WebSocket close. Clean: \(clean), code: \(code), reason: \(reason)")
+        webSocket!.event.close = { [weak self] (code, reason, clean) in
+            guard let welf = self else { return }
+            welf.logger.log(logLevel: .info, message: "WebSocket close. Clean: \(clean), code: \(code), reason: \(reason)")
+
             // the transport could have already been closed as a result of an error. In this case we should not call
             // transportDidClose again on the delegate.
-            guard !self.markTransportClosed() else {
-                self.logger.log(logLevel: .debug, message: "Transport already marked as closed due to an error - ignoring close")
+            guard !welf.markTransportClosed() else {
+                welf.logger.log(logLevel: .debug, message: "Transport already marked as closed due to an error - ignoring close")
                 return
             }
 
             if clean {
-                self.delegate?.transportDidClose(nil)
+                welf.delegate?.transportDidClose(nil)
             } else {
-                self.delegate?.transportDidClose(WebSocketsTransportError.webSocketClosed(statusCode: code, reason: reason))
+                welf.delegate?.transportDidClose(WebSocketsTransportError.webSocketClosed(statusCode: code, reason: reason))
             }
         }
 
-        webSocket!.event.error = { error in
-            self.logger.log(logLevel: .info, message: "WebSocket error. Error: \(error)")
+        webSocket!.event.error = { [weak self] error in
+            guard let welf = self else { return }
+
+            welf.logger.log(logLevel: .info, message: "WebSocket error. Error: \(error)")
             // This handler should not be called after the close event but we need to mark the transport as closed to prevent calling transportDidClose
             // on the delegate multiple times so we can as well add the check and log
-            guard !self.markTransportClosed() else {
-                self.logger.log(logLevel: .info, message: "Transport already marked as closed - ignoring error")
+            guard !welf.markTransportClosed() else {
+                welf.logger.log(logLevel: .info, message: "Transport already marked as closed - ignoring error")
                 return
             }
 
-            self.delegate?.transportDidClose(error)
+            welf.delegate?.transportDidClose(error)
         }
 
-        webSocket!.event.message = { message in
-            self.logger.log(logLevel: .debug, message: "WebSocket message")
+        webSocket!.event.message = { [weak self] message in
+            guard let welf = self else { return }
 
             if let text = message as? String {
-                self.delegate?.transportDidReceiveData(text.data(using: .utf8)!)
+                welf.delegate?.transportDidReceiveData(text.data(using: .utf8)!)
             } else {
-                self.delegate?.transportDidReceiveData(message as! Data)
+                welf.delegate?.transportDidReceiveData(message as! Data)
             }
         }
         webSocket!.open()
@@ -95,7 +101,7 @@ public class WebsocketsTransport: Transport {
 
         return url
     }
-    
+
     @inline(__always) private func populateHeaders(headers: [String : String], request: inout URLRequest) {
         headers.forEach { (key, value) in
             request.addValue(value, forHTTPHeaderField: key)
