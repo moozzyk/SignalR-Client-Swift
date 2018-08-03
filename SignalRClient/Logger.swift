afc00220754babf78054a8f66abc82aa1ef08e1d
moozzyk <moozzyk@gmail.com>
2018-08-02 19:21:42 -0700

    Adding logging infrastructure

---
+++ SignalRClient/Logger.swift
@@ -0,0 +1,59 @@
+//
+//  Logger.swift
+//  SignalRClient
+//
+//  Created by Pawel Kadluczka on 8/2/18.
+//  Copyright © 2018 Pawel Kadluczka. All rights reserved.
+//
+
+import Foundation
+
+public enum LogLevel: Int {
+    case error = 1
+    case warning = 2
+    case info = 3
+    case debug = 4
+}
+
+public protocol Logger {
+    func log(logLevel: LogLevel, message: String)
+}
+
+public extension LogLevel {
+    public func toString() -> String {
+        switch (self) {
+        case LogLevel.error: return "error"
+        case LogLevel.warning: return "warning"
+        case LogLevel.info: return "info"
+        case LogLevel.debug: return "debug"
+        }
+    }
+}
+
+public class PrintLogger: Logger {
+    public func log(logLevel: LogLevel, message: String) {
+        // TODO: time?
+        print("\(logLevel.toString()): \(message)")
+    }
+}
+
+public class NullLogger: Logger {
+    public func log(logLevel: LogLevel, message: String) {
+    }
+}
+
+class FilteringLogger: Logger {
+    private let minLogLevel: LogLevel
+    private let logger: Logger
+
+    init(minLogLevel: LogLevel, logger: Logger) {
+        self.minLogLevel = minLogLevel
+        self.logger = logger
+    }
+
+    func log(logLevel: LogLevel, message: String) {
+        if (logLevel.rawValue <= minLogLevel.rawValue) {
+            logger.log(logLevel: logLevel, message: message)
+        }
+    }
+}
---
+++ SignalRClientTests/LoggerTests.swift
@@ -0,0 +1,31 @@
+//
+//  LoggerTests.swift
+//  SignalRClientTests
+//
+//  Created by Pawel Kadluczka on 8/2/18.
+//  Copyright © 2018 Pawel Kadluczka. All rights reserved.
+//
+
+import XCTest
+@testable import SignalRClient
+
+class LoggerTests: XCTestCase {
+
+    func testThatFilteringLoggerLogsPerMinLogLevel() {
+        class TestLogger: Logger {
+            var logLevels: [LogLevel] = []
+            func log(logLevel: LogLevel, message: String) {
+                logLevels.append(logLevel)
+            }
+        }
+
+        let minLogLevel = LogLevel.warning
+        let testLogger = TestLogger()
+        let logger = FilteringLogger(minLogLevel: minLogLevel, logger: testLogger)
+
+        let logEntries: [LogLevel] = [.warning, .error, .warning, .info, .debug, .error]
+        logEntries.forEach {logger.log(logLevel: $0, message: "")}
+
+        XCTAssertEqual(logEntries.filter {$0.rawValue <= minLogLevel.rawValue}, testLogger.logLevels)
+    }
+}
