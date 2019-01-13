2decd58a01f72c594a0694a1e3d261918dc15deb
Richard Piazza <rpiazza@nerdery.com>
2019-01-13 14:05:25 -0600

    Working third-party zlib package.

--- Package.swift
+++ Package.swift
@@ -14,14 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        
+        .package(url: "https://github.com/Zewo/zlib.git", from: "0.4.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SignalRClient",
-            dependencies: ["zlib-spm"],
+            dependencies: ["zlib"],
             path: "SignalRClient"),
         .testTarget(
             name: "SignalRClientTests",
