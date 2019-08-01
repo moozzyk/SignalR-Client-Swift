a1301f0ba2f942e16816a72212126b79d8d11cae
moozzyk <moozzyk@gmail.com>
2019-08-01 13:57:31 -0700

    Updating the playground sample

--- Examples/Playground/Playground.playground/Contents.swift
+++ Examples/Playground/Playground.playground/Contents.swift
@@ -2,7 +2,7 @@ import Cocoa
 import SignalRClient
 
 let hubConnection = HubConnectionBuilder(url: URL(string: "http://localhost:5000/playground")!)
-    .withLogging(minLogLevel: .error)
+    .withLogging(minLogLevel: .info)
     .build()
 
 hubConnection.on(method: "AddMessage") {(user: String, message: String) in
@@ -10,6 +10,9 @@ hubConnection.on(method: "AddMessage") {(user: String, message: String) in
 }
 
 hubConnection.start()
+// NOTE: break here before to make this sample work and prevent errors
+// caused by trying to invoke server side methods before starting the
+// connection completed
 
 // invoking a hub method and receiving a result
 hubConnection.invoke(method: "Add", 2, 3, resultType: Int.self) { result, error in
@@ -47,4 +50,8 @@ DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
     hubConnection.cancelStreamInvocation(streamHandle: streamHandle) { error in
         print("Canceling stream invocation failed: \(error)")
     }
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
+    hubConnection.stop()
 }
