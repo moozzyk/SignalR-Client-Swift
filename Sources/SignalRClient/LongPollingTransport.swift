bc884fa5d308f3d3232f85fd5e18f95e52c3ec4e
David Robertson <david.robertson@motorolasolutions.com>
2020-07-28 20:17:13 +0100

    Remove unnecessary DispatchQueue use in LongPollingTransport

--- Sources/SignalRClient/LongPollingTransport.swift
+++ Sources/SignalRClient/LongPollingTransport.swift
@@ -12,14 +12,12 @@ public class LongPollingTransport: Transport {
     public var delegate: TransportDelegate?
     
     private let logger: Logger
-    private let pollQueue: DispatchQueue = DispatchQueue(label: "LongPollingTransport")
     
     private var active = false
     private var opened = false
     private var closeCalled = false
     private var httpClient: HttpClientProtocol?
     private var url: URL?
-    
     private var closeError: Error?
     
     init(logger: Logger) {
@@ -59,79 +57,76 @@ public class LongPollingTransport: Transport {
         if !closeCalled {
             closeCalled = true
             active = false
-            pollQueue.async {
-                self.logger.log(logLevel: .debug, message: "Sending LongPolling session DELETE request...")
-                self.httpClient?.delete(url: self.url!, completionHandler: { (_, errorOptional) in
-                    if let error = errorOptional {
-                        self.logger.log(logLevel: .error, message: "Error while DELETE-ing long polling session: \(error)")
-                        self.delegate?.transportDidClose(error)
-                    } else {
-                        self.logger.log(logLevel: .info, message: "LongPolling transport stopped.")
-                        self.delegate?.transportDidClose(self.closeError)
-                    }
-                })
-            }
+            
+            self.logger.log(logLevel: .debug, message: "Sending LongPolling session DELETE request...")
+            self.httpClient?.delete(url: self.url!, completionHandler: { (_, errorOptional) in
+                if let error = errorOptional {
+                    self.logger.log(logLevel: .error, message: "Error while DELETE-ing long polling session: \(error)")
+                    self.delegate?.transportDidClose(error)
+                } else {
+                    self.logger.log(logLevel: .info, message: "LongPolling transport stopped.")
+                    self.delegate?.transportDidClose(self.closeError)
+                }
+            })
+        } else {
+            self.logger.log(logLevel: .debug, message: "closeCalled flag is already set")
         }
     }
     
     
     // MARK: - Private methods
+
     
     private func triggerPoll() {
-        self.pollQueue.async {
-            if self.active {
-                self.poll()
-            } else {
-                self.logger.log(logLevel: .debug, message: "Long Polling transport polling complete.")
-                self.close()
-            }
+        if self.active {
+            let pollUrl = self.getPollUrl()
+            self.logger.log(logLevel: .debug, message: "Polling \(pollUrl)")
+            self.httpClient?.get(url: pollUrl, completionHandler: self.handlePollResponse(response:error:))
+        } else {
+            self.logger.log(logLevel: .debug, message: "Long Polling transport polling complete.")
+            self.close()
         }
     }
     
-    private func poll() {
-        let pollUrl = getPollUrl()
-        logger.log(logLevel: .debug, message: "Polling \(pollUrl)")
-        
-        httpClient?.get(url: pollUrl, completionHandler: { (responseOptional, errorOptional) in
-            if let error = errorOptional {
-                if (error as? URLError)?.errorCode == NSURLErrorTimedOut {
-                    self.logger.log(logLevel: .debug, message: "Poll timed out (client side), reissuing.")
-                } else {
-                    self.logger.log(logLevel: .error, message: "Error during polling: \(error)")
-                    self.closeError = error
-                    self.active = false
-                }
-                
-            } else if let response = responseOptional {
-                switch response.statusCode {
-                case 204:
-                    self.logger.log(logLevel: .info, message: "LongPolling transport terminated by server.")
-                    self.closeError = nil
-                    self.active = false
-                    
-                case 200:
-                    if !self.opened {
-                        // First response must be discarded.
-                        self.opened = true
-                        self.delegate?.transportDidOpen()
-                    } else if let data = response.contents, data.count > 0 {
-                        self.logger.log(logLevel: .debug, message: "Message received: \(data)")
-                        self.delegate?.transportDidReceiveData(data)
-                    } else {
-                        self.logger.log(logLevel: .debug, message: "Poll timed out (server side), reissuing.")
-                    }
-                    
-                default:
-                    self.logger.log(logLevel: .error, message: "Unexpected response code \(response.statusCode)")
-                    self.closeError = SignalRError.webError(statusCode: response.statusCode)
-                    self.active = false
-                }
+    private func handlePollResponse(response: HttpResponse?, error: Error?) {
+        if let error = error {
+            if (error as? URLError)?.errorCode == NSURLErrorTimedOut {
+                self.logger.log(logLevel: .debug, message: "Poll timed out (client side), reissuing.")
+            } else {
+                self.logger.log(logLevel: .error, message: "Error during polling: \(error)")
+                self.closeError = error
+                self.active = false
             }
             
-            self.triggerPoll()
-        })
+        } else if let response = response {
+            switch response.statusCode {
+            case 204:
+                self.logger.log(logLevel: .info, message: "LongPolling transport terminated by server.")
+                self.closeError = nil
+                self.active = false
+                
+            case 200:
+                if !self.opened {
+                    // First response must be discarded.
+                    self.opened = true
+                    self.delegate?.transportDidOpen()
+                } else if let data = response.contents, data.count > 0 {
+                    self.logger.log(logLevel: .debug, message: "Message received: \(data)")
+                    self.delegate?.transportDidReceiveData(data)
+                } else {
+                    self.logger.log(logLevel: .debug, message: "Poll timed out (server side), reissuing.")
+                }
+                
+            default:
+                self.logger.log(logLevel: .error, message: "Unexpected response code \(response.statusCode)")
+                self.closeError = SignalRError.webError(statusCode: response.statusCode)
+                self.active = false
+            }
+        }
         
+        self.triggerPoll()
     }
+    
     
     private func getPollUrl() -> URL {
         var components = URLComponents.init(url: self.url!, resolvingAgainstBaseURL: true)!
