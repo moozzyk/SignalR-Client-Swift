56d1967d67e8b2627d543c2b8c71bd1e773e44aa
Marwen Doukh <marwen@doukh.tn>
2023-07-26 07:03:14 +0200

    Fixing a bug when stopping a disconnected connection (#283)
    * fixing a bug when stopping a disconnected connection
    
    * add stop connection when disconnected unit test
    
    * check disconnected state refactoring

--- Sources/SignalRClient/ReconnectableConnection.swift
+++ Sources/SignalRClient/ReconnectableConnection.swift
@@ -71,8 +71,11 @@ internal class ReconnectableConnection: Connection {
 
     func stop(stopError: Error?) {
         logger.log(logLevel: .info, message: "Received connection stop request")
-        _ = changeState(from: nil, to: .stopping)
-        underlyingConnection.stop(stopError: stopError)
+        if changeState(from: [.starting, .reconnecting, .running], to: .stopping) != nil {
+          underlyingConnection.stop(stopError: stopError)
+        } else {
+          logger.log(logLevel: .warning, message: "Reconnectable connection is already in the disconnected state. Ignoring stop request")
+        }
     }
 
     private func startInternal() {
