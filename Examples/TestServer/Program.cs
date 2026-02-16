a5aca80a4de24cc59bb51a1a9c7242960422b9b3
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2026-02-16 15:54:00 -0800

    Refactor TestServer startup to Generic Host pattern

--- Examples/TestServer/Program.cs
+++ Examples/TestServer/Program.cs
@@ -1,21 +1,25 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace TestServer
 {
     public class Program
     {
-        public static void Main(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
                 .ConfigureLogging(factory =>
                 {
-                    factory.AddConsole()
-                        .SetMinimumLevel(LogLevel.Debug);
+                    factory.AddConsole();
+                    factory.SetMinimumLevel(LogLevel.Debug);
                 })
-                .UseStartup<Startup>()
-                .UseUrls("http://0.0.0.0:5000/;https://0.0.0.0:5001/")
-                .Build()
-                .Run();
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .UseUrls("http://0.0.0.0:5000/;https://0.0.0.0:5001/");
+                });
     }
 }
